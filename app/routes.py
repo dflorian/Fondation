f304cc47ac18bfa087d716c59fef5be85daaa75a
Unknown <xavier.bollart@gmail.com>
2019-08-30 20:31:35 -0400

    apply template on signup and signin pages

--- app/routes.py
+++ app/routes.py
@@ -10,38 +10,33 @@ from flask import request
 from werkzeug.urls import url_parse
 from app import db
 from app.forms import RegistrationForm
-
-from flask_mail import Message
-# from app.forms import ResetPasswordRequestForm
 from app.email import send_first_contact_email
-# from app.forms import ResetPasswordForm
-from app import mail
 
-@app.route("/home/")
+@app.route("/home")
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/about/")
+@app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect(url_for('signin'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('home')
         return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('signin.html', title='Sign in', form=form)
 
 @app.route('/logout')
 def logout():
@@ -53,12 +48,7 @@ def logout():
 def listing():
     return render_template("listing.html")
 
-@app.route('/index')
-@login_required
-def index():
-    return render_template("index.html", title='Home Page')
-
-@app.route("/contact/", methods=['GET', 'POST'])
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
     form = ContactForm()
     if form.validate_on_submit():
@@ -68,14 +58,14 @@ def contact():
         return redirect(url_for('contact'))
     return render_template("contact.html", title='Contact', form=form)
 
-@app.route("/faq/")
+@app.route("/faq")
 def faq():
     return render_template("faq.html")
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -83,37 +73,8 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
-
-# @app.route('/reset_password_request', methods=['GET', 'POST'])
-# def reset_password_request():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('index'))
-#     form = ResetPasswordRequestForm()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(email=form.email.data).first()
-#         if user:
-#             send_password_reset_email(user)
-#         flash('Check your email for the instructions to reset your password')
-#         return redirect(url_for('login'))
-#     return render_template('reset_password_request.html',
-#                            title='Reset Password', form=form)
-#
-# @app.route('/reset_password/<token>', methods=['GET', 'POST'])
-# def reset_password(token):
-#     if current_user.is_authenticated:
-#         return redirect(url_for('index'))
-#     user = User.verify_reset_password_token(token)
-#     if not user:
-#         return redirect(url_for('index'))
-#     form = ResetPasswordForm()
-#     if form.validate_on_submit():
-#         user.set_password(form.password.data)
-#         db.session.commit()
-#         flash('Your password has been reset.')
-#         return redirect(url_for('login'))
-#     return render_template('reset_password.html', form=form)
+        return redirect(url_for('signin'))
+    return render_template('signup.html', title='Sign up', form=form)
 
 @app.errorhandler(404)
 def not_found_error(error):
